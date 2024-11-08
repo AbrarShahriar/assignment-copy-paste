ecea1d4104932dd80161737ddf168ccd8f83092a
AbrarShahriar <AbrarShahriar321@gmail.com>
2024-11-09 00:47:35 +0600

    feat: initial commit

---
+++ GenerateTxt.java
@@ -0,0 +1,97 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileFilter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class GenerateTxt {
+    public static String studentName = "";
+    public static String assignmentNumber = "";
+    public static String studentId = "";
+    public static String folderName = "";
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Taking User Input
+        System.out.print("Student Name: ");
+        studentName = sc.nextLine();
+        System.out.print("Student ID: ");
+        studentId = sc.next();
+        System.out.print("Assignment Number: ");
+        assignmentNumber = sc.next();
+        System.out.print("Folder Name: ");
+        folderName = sc.next();
+
+        String fileName = "Assignment " + assignmentNumber + "_" + studentId + "_" + studentName + ".txt";
+
+        // Setup the output file
+        File outputFile = createTxtFile(fileName);
+        try {
+            FileWriter writer = new FileWriter(outputFile, true);
+
+            // Reading files from directory
+            File folder = new File("./" + folderName);
+            if (folder.isDirectory()) {
+                // Filtering only the .java files
+                FileFilter filter = file -> file.getName().startsWith("Task") && file.getName().endsWith(".java");
+                File[] files = folder.listFiles(filter);
+
+                // Looping through every java file
+                if (files != null) {
+                    for (int i = 0; i < files.length; i++) {
+                        FileReader reader = new FileReader(files[i]);
+                        BufferedReader bufferedReader = new BufferedReader(reader);
+                        String line;
+
+                        writer.write("#Task" + ((int) i + 1) + ":");
+                        writer.write("\n");
+                        // Read each line from the file and write to the output file
+                        while ((line = bufferedReader.readLine()) != null) {
+                            writer.write(line);
+                            writer.write("\n"); // Newline after each line
+                        }
+
+                        writer.write("\n"); // Add a newline after the content of each file
+                        bufferedReader.close();
+                    }
+                } else {
+                    System.out.println("No files found or an error occurred.");
+                }
+            } else {
+                System.out.println("The provided path is not a directory.");
+            }
+
+            writer.close(); // Close the FileWriter
+            System.out.println("Content written to " + fileName + " successfully!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        sc.close();
+    }
+
+    public static void readFiles(File[] files) {
+        for (int i = 0; i < files.length; i++) {
+            System.out.println(files[0]);
+
+        }
+    }
+
+    public static File createTxtFile(String fileName) {
+        File outputFile = new File(fileName);
+
+        if (!outputFile.exists()) {
+            try {
+                outputFile.createNewFile();
+                System.out.println("File written successfully!");
+            } catch (IOException e) {
+                System.err.println("Error writing to file: " + e.getMessage());
+            }
+        }
+
+        return outputFile;
+    }
+}
---
+++ README.md
@@ -0,0 +1,75 @@
+# BRACU CSE assignment copy paster
+
+<p align="center">
+Copy pasting each java assignment from different files is cumbersome and annoying. So, I created this little script to automate this process.
+</p>
+
+<p align="center">
+  <img src="https://img.shields.io/badge/Java-v7+-teal?style=for-the-badge">&nbsp;
+  <img src="https://img.shields.io/badge/MIT-green?style=for-the-badge">&nbsp;
+</p>
+
+## 📝 Requirements
+
+> <br>
+> <strong>JAVA version 7 or up</strong> <br>
+> <br>
+
+<br>
+
+This script was made and tested in **JAVA version 22**
+
+## ⚠️ READ THIS!!
+
+You have to follow certain rules to not run into any errors.
+
+- Your files have to follow this naming convention: `TaskX.java`
+
+```
+Task1.java
+Task2.java
+Task3.java
+...
+```
+
+- Place all your assignment `.java` files in a new folder e.g. `assignment1`
+- Place `GenerateTxt.java` and `assignment1` next to each other:
+
+```
+root
+├── GenerateTxt.java
+└── assignment1
+    └── Task1.java
+    └── Task2.java
+    └── Task3.java
+    └── ...
+    └── TaskX.java
+```
+
+- The generated `.txt` file will have this format: `Assignment XX_studentID_YourName.txt`. This is the current assignment file naming convention BRACU is following.
+
+## ⚡ Usage
+
+1. Make sure the script file and assignment folder are in the same parent folder
+2. Compile `GenerateTxt.java` by running this command:
+
+```bash
+javac GenerateTxt.java
+```
+
+Go through each prompt:
+
+```bash
+Student Name: Your Name
+Student ID: 24301500
+Assignment Number: 01
+Folder Name: assignment1
+Content written to Assignment 01_24301500_Your Name.txt successfully!
+```
+
+The above command will generate a file named
+`Assignment 01_24301500_Your Name.txt` in the parent folder next to `GenerateTxt.java`.
+
+## Before you go
+
+Feel free to modify the script to fit your needs.
